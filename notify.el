4e2b54c6542f96b1dcd1a82c0c82755ff19f15ad
Le Tan Khoa <tkhoa2711@yahoo.com.vn>
2015-08-18 00:11:48 +0800

    first version with notification via emacs minibuffer and growl

---
+++ notify.el
@@ -0,0 +1,37 @@
+;;; notify.el - A minimal library for notification in elisp
+
+(defvar notify-function 'notify-minibuffer
+  "Function to send notification.
+Set this variable to your preferred notification function.")
+
+(defvar notify-growl-executable 
+  (cond
+   ((string-equal system-type "windows-nt")
+    "growlnotify.com")
+   ((string-equal system-type "darwin")
+    "growlnotify")
+   ((string-equal system-type "gnu/linux")
+    "gol"))
+  "Set the path to growlnotify if it isn't executable within your PATH environment")
+
+(defun notify (title body)
+  "Send notification with title and body."
+  (funcall notify-function title body))
+
+(defun notify-minibuffer (title body)
+  "Show TITLE and BODY in the minibuffer."
+  (message "Notification: [%s] %s" title body))
+
+(defun notify-growl (title body)
+  "Send message with TITLE and BODY via Growl."
+  (start-process "growl"
+		 "growl"
+		 notify-growl-executable
+		 "-t"
+		 title
+		 "-m"
+		 body))
+
+(provide 'notify)
+
+;;; notify.el ends here
